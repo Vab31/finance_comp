1342e3d67618f0d4b9a3e2513aef25474d77cad1
Vaibhav Singh <singhvaibhav654@gmail.com>
2025-03-25 10:34:28 +0530

    first commit

---
+++ src/app/component/home/hero.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div><section class="text-gray-600 body-font bg-black">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4  text-white font-bold">Title Heading
+          <br class="hidden lg:inline-block"/>Subheading in single line
+        </h1>
+        <p class="mb-4 leading-relaxed text-6xl font-serif text-gray-200">Asia Pacific Edition</p>
+    <p class="mb-3 leading-relaxed text-3xl font-serif text-gray-200">22 April 2025<br/>
+        9:00 AM CET / 1:30 PM IST</p>
+
+        <div class="flex justify-center">
+          <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
+        </div>
+      </div>
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 border-2 rounded-2xl border-white shadow-amber-100">
+        <img class="object-cover object-center rounded-2xl" alt="hero" src="images/image.png"/>
+      </div>
+    </div>
+  </section>
+  </div>
+  )
+}
---
+++ src/app/component/home/time.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Time() {
+  return (
+<div class="text-center flex justify-center items-center bg-black py-5">
+    <div class="flex items-center space-x-3">
+        <img src='/images/iconloud.png' />
+        <div class="text-white">Join us live on April 22nd, 9:00 AM CET / 1:30 PM IST</div>
+    </div>
+</div>
+
+
+
+  )
+}
